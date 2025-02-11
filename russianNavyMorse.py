afb94acebf864ceba9d0bcc3d34df48b72c0024f
User <Willspri@gmail.com>
2025-02-11 13:53:02 +1300

    updated

--- russianNavyMorse.py
+++ russianNavyMorse.py
@@ -7,3 +7,18 @@ morseDict = {
     '...-..-': '$', '.--.-.': '@', '-.--.-': '(', '-.--.': ')', '.-...': '&', '-.-.-.': ';'
 }
 
+def english_morse(morse_code):
+    words = morse_code.split(' / ')
+    translated_chars = []
+    
+    for word in words:
+        chars = word.split()
+        translated_char = ''.join(morseDict.get(char, '?') for char in chars)
+        translated_chars.append(translated_char)
+        
+    return ' '.join(translated_chars)
+    
+if __name__ == "__main__":
+    morse_code = input("Enter Navy Code: ")
+    translation = english_morse(morse_code)
+    print("Translated Text:", translation)
